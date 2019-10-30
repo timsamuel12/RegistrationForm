1e0391340a047188c795919139f38681fa795b73
timsamuel12 <timsamuel12@gmail.com>
2019-10-30 08:58:41 -0400

    Set up PHP environment and HTML basics.

--- registration.php
+++ registration.php
@@ -1,1 +1,19 @@
 <?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset ="UTF-8">
+    <title>Registration Form</title>
+</head>
+<body>
+    <h1>Register Below</h1>
+
+    <form method = "post">
+        
+    </form>
+</body>
+</html>
