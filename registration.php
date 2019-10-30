090dfc7eac95572d46ac7c9c884c9673ac8ac9c8
timsamuel12 <timsamuel12@gmail.com>
2019-10-30 09:04:45 -0400

    Form with all fields

--- registration.php
+++ registration.php
@@ -13,7 +13,17 @@
     <h1>Register Below</h1>
 
     <form method = "post">
-        
-    </form>
+    First Name:
+        <input type ="text" name = "fname" placeholder = "Enter First Name"><br><br>
+    Last Name:
+        <input type ="text" name = "lname" placeholder ="Enter Last Name"><br><br>
+    Date:
+        <input type ="date" name ="date" placeholder = "Enter Date of Birth"><br><br>
+    Email:
+        <input type ="text" name="email" placeholder ="Enter a valid email address"><br><br>
+    Password:
+        <input type ="text" name="pass" placeholder ="Enter desired password"><br><br>
+    <input type ="submit" name ="submit" value ="Registration">
+    </form>"
 </body>
 </html>
